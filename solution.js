ff632a1d22fb501464f7606dcce6d73ccf59c3b6
Sarthak Negi <sarthakn658@gmail.com>
2026-03-17 20:54:28 +0530

    Update solution.js

--- solution.js
+++ solution.js
@@ -4,3 +4,16 @@ for(i= str.length-1; i>=0; i--)
     {
     console.log(`output of reverse string is ${str[i]}`)
 }
+
+//find the string is polindrome or not
+let str = "racecar";
+let b  = "";
+for(i = str.length-1; i>=0;i--){
+    b+=str[i];
+}
+if(b===str){
+    console.log(`the value is polindrome`)
+}
+else{
+    console.log(`the value is not polindrome`)
+}
